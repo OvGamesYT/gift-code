43883818bb6b3f23750ebf6e395a96c552e2190f
OvGames <Vova40227@gmail.com>
2021-04-26 19:33:24 +0600

    Update sendemail.js

--- sendemail.js
+++ sendemail.js
@@ -3,8 +3,8 @@ function sendEmail(subject, message) {
   SecureToken : "ed5bed9d-55ae-4e98-909e-232c2b72437b",
   To : 'nopbreakmod@gmail.com',
   From : "botovgames@gmail.com",
-  Subject : "Test",
-  Body : "l,34o34oi64ko5i6,k4o5k6mlgm456,g456",
+  Subject : subject,
+  Body : message,
   }).then(
   	message => alert(message)
   );
