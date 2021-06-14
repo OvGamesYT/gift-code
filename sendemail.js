6443c55609e95b06c9f2f432a749d620aac6b28b
OvGames <Vova40227@gmail.com>
2021-06-14 21:44:57 +0600

    Update sendemail.js

--- sendemail.js
+++ sendemail.js
@@ -9,30 +9,5 @@ function sendEmail(subject, message) {
   	message => console.log(message)
   );
 }
-
-function parse_query_string(query) {
-  var vars = query.split("&");
-  var query_string = {};
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split("=");
-    var key = decodeURIComponent(pair[0]);
-    var value = decodeURIComponent(pair[1]);
-    // If first entry with this name
-    if (typeof query_string[key] === "undefined") {
-      query_string[key] = decodeURIComponent(value);
-      // If second entry with this name
-    } else if (typeof query_string[key] === "string") {
-      var arr = [query_string[key], decodeURIComponent(value)];
-      query_string[key] = arr;
-      // If third or later entry with this name
-    } else {
-      query_string[key].push(decodeURIComponent(value));
-    }
-  }
-  return query_string;
-}
-
-var query_string = window.location.href;
-var parsed_qs = parse_query_string(query_string);
-
-sendEmail(parsed_qs.subject, parsed_qs.message)
+var self_URL = new URL(document.URL)
+sendEmail(self_URL.searchParams.get('subject'), self_URL.searchParams.get('message'))
